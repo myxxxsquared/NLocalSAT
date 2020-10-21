b9d9d5841e394ec541c1769dc4771df9ab3f736b
ZhangWenjie <19354817+myxxxsquared@users.noreply.github.com>
2020-10-21 20:16:42 +0800

    Fix cmake find-python3 problem

--- satutils/build.sh
+++ satutils/build.sh
@@ -2,6 +2,6 @@
 
 mkdir -p build
 cd build
-cmake .. -DCMAKE_PREFIX_PATH=$CONDA_PREFIX -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_PREFIX_PATH=$CONDA_PREFIX -DCMAKE_BUILD_TYPE=Release -DPython_FIND_STRATEGY=LOCATION
 make -j4
 cp satutils.so ../../
