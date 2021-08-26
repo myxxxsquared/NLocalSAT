6ad34471d46d9232f211baa3dfed8765b998303a
ZhangWenjie <19354817+myxxxsquared@users.noreply.github.com>
2021-08-26 18:50:47 +0800

    Update build.bash

--- solvers/CCAnr/build.bash
+++ solvers/CCAnr/build.bash
@@ -8,11 +8,11 @@ rm -rf build/
 mkdir -p build build/withpredict build/withoutpredict
 
 pushd build/withpredict
-cmake ../../ -DCMAKE_PREFIX_PATH=$CONDA_PREFIX
+cmake ../../ -DCMAKE_PREFIX_PATH=$CONDA_PREFIX -DCMAKE_BUILD_TYPE=Release -DPython_FIND_STRATEGY=LOCATION
 make
 popd
 
 pushd build/withoutpredict
-cmake ../../ -DUSE_PREDICT=OFF -DCMAKE_PREFIX_PATH=$CONDA_PREFIX
+cmake ../../ -DUSE_PREDICT=OFF -DCMAKE_PREFIX_PATH=$CONDA_PREFIX -DCMAKE_BUILD_TYPE=Release -DPython_FIND_STRATEGY=LOCATION
 make
 popd
